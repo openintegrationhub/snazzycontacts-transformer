7554305bd88b218aeabf8251a0c47b4536af3fb3
Shterion <shterion.yanev@yahoo.com>
2018-08-22 14:48:28 +0200

    Add a seed file

--- spec/transform.spec.js
+++ spec/transform.spec.js
@@ -3,115 +3,15 @@
 const expect = require('chai').expect;
 const transform = require('../lib/actions/transformPersonFromOih');
 const eioUtils = require('elasticio-node').messages;
+const personExpression = require('seed/person');
 
 describe('Transformation test', () => {
 
   it('should handle simple transforms', () => {
-    const exp = {
-      "title": "Prof.",
-      "salutation": "Mr.",
-      "firstName": "John",
-      "middleName": "Anthony",
-      "lastName": "Doe",
-      "gender": "male",
-      "birthday": "Wed, 14 Jun 1999 07:00:00 GMT",
-      "notes": "Private notes",
-      "displayName": "johndoe",
-      "language": "english",
-      "nickname": "johny",
-      "jobTitle": "Sales manager",
-      "photo": "http://example.org/photo.jpg",
-      "anniversary": "14 Jun",
-      "addresses": [{
-          "street": "Hohestr",
-          "streetNumber": "3",
-          "unit": "a",
-          "zipCode": "50667",
-          "city": "Cologne",
-          "district": "Alstadt-Sued",
-          "region": "NRW",
-          "country": "Germany",
-          "primaryContact": "Hermann Schmitz",
-          "description": "primary"
-        },
-        {
-          "street": "Rudolfplatz",
-          "streetNumber": "3",
-          "unit": "a",
-          "zipCode": "50667",
-          "city": "Cologne",
-          "district": "Alstadt-Sued",
-          "region": "NRW",
-          "country": "Germany",
-          "primaryContact": "Hermann Schmitz",
-          "description": "mailing"
-        }
-      ],
-      "contactData": [{
-          "value": "123456789",
-          "type": "phone",
-          "description": "primary"
-        },
-        {
-          "value": "00224477",
-          "type": "phone",
-          "description": "private"
-        },
-        {
-          "value": "95248793",
-          "type": "phone",
-          "description": "mobile"
-        },
-        {
-          "value": "jon@doe.com",
-          "type": "email",
-          "description": "private"
-        },
-        {
-          "value": "xing.de/yourUsername",
-          "type": "xing",
-          "description": "xing"
-        },
-        {
-          "value": "98326307",
-          "type": "phone",
-          "description": "secondary"
-        }
-      ],
-      "calendar": [{
-        "calendar": "http://example.org/kalender/emuster",
-        "busyCalendar": "http://example.org/kalender/emuster/busy",
-        "requestCalendar": "http://example.org/kalender/emuster/appointment",
-        "description": "private"
-      }],
-      "category": [{
-        "name": "private",
-        "description": "private address data of the person"
-      }],
-      "oihApplicationRecords": [{
-          "applicationUid": 3,
-          "recordUid": 98765,
-          "created": 23215151,
-          "lastModified": 36542364
-        },
-        {
-          "applicationUid": 2,
-          "recordUid": 12121,
-          "created": 454545454,
-          "lastModified": 7878787
-        }
-      ]
-    };
-
     return transform.process(eioUtils.newMessageWithBody(exp))
     .then(result => {
       console.log(`RESULT: ${JSON.stringify(result, undefined, 2)}`);
       expect(result.body).to.be.an('object');
-      // expect(result.body).to.deep.include({
-      //   firstName: "John",
-      //   middleName: "Anthony",
-      //   lastName: "Doe"
-      // });
     });
   });
 
