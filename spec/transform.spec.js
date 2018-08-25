55f65c09bf1f1aa3a54445def6f198ea7292e1b4
Shterion <shterion.yanev@yahoo.com>
2018-08-25 21:06:59 +0200

    Refactoring

--- spec/transform.spec.js
+++ spec/transform.spec.js
@@ -1,14 +1,13 @@
 /* eslint-disable arrow-body-style */
+/* eslint-disable no-unused-expressions */
 
-const { expect } = require('chai');
-const { assert } = require('chai');
-const { messages } = require('elasticio-node');
-const { personFromOih, personToOih } = require('./seed/person');
+const {expect} = require('chai');
+const {messages} = require('elasticio-node');
+const {personFromOih, personToOih} = require('./seed/person');
 const transformPersonFromOih = require('../lib/actions/transformPersonFromOih');
 const transformPersonToOih = require('../lib/actions/transformPersonToOih');
 
 describe('Transformation test', () => {
-
   it('should handle simple person tranformation in direction from OIH', () => {
     const exp = personFromOih();
     return transformPersonFromOih.process(messages.newMessageWithBody(exp))
@@ -16,20 +15,20 @@ describe('Transformation test', () => {
         expect(result.body).to.be.an('object');
         expect(result.body).to.deep.include({
           rowid: 98765,
-          name: "Doe",
-          firstname: "John",
-          position: "Sales manager",
-          private_street: "Hohestr",
-          private_street_number: "3",
-          private_zip_code: "50667",
-          private_town: "Cologne",
-          private_country: "Germany",
-          email: "jon@doe.com",
-          phone: "123456789",
-          mobile_phone: "98326307",
-          xing_url: "xing.de/yourUsername",
+          name: 'Doe',
+          firstname: 'John',
+          position: 'Sales manager',
+          private_street: 'Hohestr',
+          private_street_number: '3',
+          private_zip_code: '50667',
+          private_town: 'Cologne',
+          private_country: 'Germany',
+          email: 'jon@doe.com',
+          phone: '123456789',
+          mobile_phone: '98326307',
+          xing_url: 'xing.de/yourUsername',
           last_update: 36542364
-        })
+        });
       });
   });
 
@@ -46,20 +45,21 @@ describe('Transformation test', () => {
       .then(result => {
         expect(result.body).to.be.an('object');
         expect(result.body).to.deep.include({
-          firstName: "Mark",
-          lastName: "Smith",
-          jobTitle: "Marketing Manager",
+          firstName: 'Mark',
+          lastName: 'Smith',
+          jobTitle: 'Marketing Manager'
         });
-        assert(Array.isArray([result.body.addresses], 'It is an array'));
-        assert.deepEqual(result.body.addresses[0].street, 'Main Str.');
-        assert.deepEqual(result.body.addresses[0].streetNumber, 120);
-        // expect(result.body.addresses[0]).to.deep.include({
-        //   street: 'Main Str.',
-        //   streetNumber: 120
-        // })
+        expect(result.body.test).to.be.undefined;
+        expect(result.body.oihApplicationRecords[0]).to.deep.include({
+          recordUid: 98123
+        });
+        expect(result.body.addresses).to.be.an('array');
+        expect(result.body.addresses[0]).to.deep.include({
+          street: 'Main Str.',
+          streetNumber: 120
+        });
       });
   });
-
 
   it('should not produce an empty message if transformation returns undefined', () => {
     return transformPersonToOih.process(messages.newMessageWithBody({}))
@@ -67,5 +67,4 @@ describe('Transformation test', () => {
         expect(result).to.be.undefined;
       });
   });
-
 });
